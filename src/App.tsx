76b821256100a4c627552ece70c45ad3130f4246
Randolph Stokes <resavt@udel.edu>
2024-08-30 17:27:42 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// Randolph Stokes
+
 import React from "react";
 import "./App.css";
 
@@ -5,11 +7,11 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript by Randolph Stokes
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. It not work for some reason.
             </p>
         </div>
     );
