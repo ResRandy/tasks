d3ba0f36494f679f45f6558c937c4d9649f4fc15
Randolph Stokes <resavt@udel.edu>
2024-09-06 16:52:01 -0400

    did task 3 and possibly 2 at the same time :(

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-// Randolph Stokes
-
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+// inserting the image file
+import car from "./Images/Wallpaper.jpg";
 
 function App(): React.JSX.Element {
     return (
@@ -9,10 +10,83 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript by Randolph Stokes
             </header>
+            <div style={{ backgroundColor: "violet", padding: "6px" }}>
+                <h1>Things that I like</h1>
+            </div>
+            <div>
+                {" "}
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    padding: "6px",
+                                    backgroundColor: "black",
+                                    height: "200px",
+                                    width: "300px",
+                                }}
+                            >
+                                <Button
+                                    onClick={() => {
+                                        console.log("Hello World!");
+                                    }}
+                                >
+                                    {" "}
+                                    Log Hello World
+                                </Button>
+                                <div
+                                    style={{
+                                        backgroundColor: "red",
+                                        height: "20px",
+                                        width: "50px",
+                                    }}
+                                ></div>
+                            </div>
+                        </Col>{" "}
+                        <Col>
+                            <div
+                                style={{
+                                    padding: "6px",
+                                    backgroundColor: "yellow",
+                                    height: "200px",
+                                    width: "500px",
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        backgroundColor: "red",
+                                        height: "20px",
+                                        width: "50px",
+                                    }}
+                                ></div>
+                                <ul>
+                                    <li>Cars</li>
+                                    <li>Video games</li>
+                                    <li>Art(including music)</li>
+                                    <li>Nature</li>
+                                </ul>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+            <div style={{ border: "4px solid blue", padding: "6px" }}>
+                This is surrounded by a border although there is a lot of
+                problems that have been created whenever I try to use Buttons
+                and Containers
+            </div>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. It not work for some reason.
             </p>
+            <div>
+                <img
+                    src={car}
+                    alt="Picture of Subaru BRZ 1st generation (car)"
+                />
+            </div>
         </div>
     );
 }
